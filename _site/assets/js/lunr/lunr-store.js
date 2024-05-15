e89ad27169bfe65249e73fa27e2e21b77bd114ff
wongyah <wongyah@163.com>
2024-05-15 11:40:34 +0800

    自定义博文格式： 1. 添加头图。 2. 调整内容页面的边距。

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -6,7 +6,7 @@ var store = [{
         "teaser": null
       },{
         "title": "我的GitHub资源库：神奇的技术写作研习路",
-        "excerpt":"引言      标题   标题   标题   标题   正文   标题   正文   标题   正文  ","categories": [],
+        "excerpt":"引言      标题   标题   标题   标题      [!TIP] 这是一条提示信息。    标题   正文   标题   正文  ","categories": [],
         "tags": [],
         "url": "/2024/04/25/awesome-technical-writing-learning.html",
         "teaser": null
